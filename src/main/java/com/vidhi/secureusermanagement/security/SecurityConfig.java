19470bef13eeec80847ec244e791c4f389624d34
Vidhi03826 <vidh312@gmail.com>
2026-09-06 00:47:19 +0530

    Finalize production configuration

--- src/main/java/com/vidhi/secureusermanagement/security/SecurityConfig.java
+++ src/main/java/com/vidhi/secureusermanagement/security/SecurityConfig.java
@@ -87,7 +87,8 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/actuator/health"
                         )
                         .permitAll()
 
